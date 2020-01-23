24f66a493c8a22a1afcfc1c4f64e045977704100
obland <59997719+obland@users.noreply.github.com>
2020-01-23 00:31:26 +0000

    Update Lab01Code.py

--- Lab01Basics/Lab01Code.py
+++ Lab01Basics/Lab01Code.py
@@ -36,7 +36,10 @@ def encrypt_message(K, message):
     plaintext = message.encode("utf8")
     
     ## YOUR CODE HERE
-
+    aes_cipher = Cipher("aes-128-gcm")
+    iv = urandom(16)
+    ciphertext, tag = aes.quick_gcm_enc(K, iv, plaintext)
+    
     return (iv, ciphertext, tag)
 
 def decrypt_message(K, iv, ciphertext, tag):
@@ -45,7 +48,11 @@ def decrypt_message(K, iv, ciphertext, tag):
         In case the decryption fails, throw an exception.
     """
     ## YOUR CODE HERE
-
+    try:
+        plain = aes.quick_gcm_dec(K, iv, ciphertext, tag)
+    except:
+        print("Decryption failed!")
+    
     return plain.encode("utf8")
 
 #####################################################
